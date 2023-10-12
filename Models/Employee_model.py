25e67d66b784faadf5f21e30e115e10d6ca67df7
Noa Dudai <nooninana@gmail.com>
2023-10-12 16:16:47 +0300

    preferences list in employee model

--- Models/Employee_model.py
+++ Models/Employee_model.py
@@ -1,4 +1,7 @@
 from enum import Enum
+from typing import List
+
+from Models.Day_model import DayModel
 
 
 #  Priority levels
@@ -12,13 +15,17 @@ class PriorityEnum(Enum):
 
 class EmployeeModel:
 
-    def __init__(self, name: str, priority: PriorityEnum, is_new: bool):
+    def __init__(self, name: str, priority: PriorityEnum, is_new: bool, preferences: List[DayModel]):
         self.name = name
         self.priority = priority
         self.is_new = is_new
+        self.preferences = preferences
 
     def update_priority(self, new_priority: PriorityEnum):
         self.priority = new_priority
 
     def update_status(self, status: bool):
         self.is_new = status
+
+    def update_preferences(self, new_preferences: List[DayModel]):
+        self.preferences = new_preferences
