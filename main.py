f8eeaba1206f6d4ddcb57b5c67f43d4f04dffb8f
Noa Dudai <nooninana@gmail.com>
2024-04-24 19:57:40 +0300

    draft

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import datetime
 from uuid import uuid4
 from ortools.sat.python import cp_model
 
+
 from constraints_file import *
 from models.employees.employee import Employee
 from models.employees.employee_priority_enum import EmployeePriorityEnum
@@ -11,6 +12,7 @@ from models.shifts.shift_combinations_key import ShiftCombinationsKey
 from models.shifts.shift import Shift
 from models.shifts.shifts_file import week_21_to_27_to_april
 from models.shifts.shifts_types_enum import ShiftTypesEnum
+from test.var_array_solution_printer import VarArraySolutionPrinter
 
 
 def create_schedule(employees: list[Employee], shifts: list[Shift]) -> list[tuple[Employee, Shift]]:
@@ -21,20 +23,15 @@ def create_schedule(employees: list[Employee], shifts: list[Shift]) -> list[tupl
 
     all_shifts = generate_shift_employee_combinations(employees, shifts, constraint_model)
     add_exactly_one_employee_per_shift_constraint(shifts, employees, constraint_model, all_shifts)
-    # add_limit_employees_working_days_constraint(shifts, employees, constraint_model, all_shifts, max_working_days)
-    afternoon_shift_keys = add_minimum_time_between_a_morning_shift_and_the_shift_before_constraint(shifts, employees, constraint_model, all_shifts, datetime.timedelta(hours=9), early_morning_start_time=datetime.time(6), afternoon_start_time=datetime.time(12, 30))
+    add_limit_employees_working_days_constraint(shifts, employees, constraint_model, all_shifts, max_working_days)
+    add_minimum_time_between_a_morning_shift_and_the_shift_before_constraint(shifts, employees, constraint_model, all_shifts, datetime.timedelta(hours=9), early_morning_start_time=datetime.time(6), afternoon_start_time=datetime.time(12, 30))
     add_prevent_overlapping_shifts_for_employees_constraint(shifts, employees, constraint_model, all_shifts)
     add_aspire_for_minimal_deviation_between_employees_position_and_number_of_shifts_given_constraint(shifts, employees, constraint_model, all_shifts)
-    add_aspire_to_maximize_all_employees_preferences_constraint(shifts, employees, constraint_model, all_shifts)
+    # add_aspire_to_maximize_all_employees_preferences_constraint(shifts, employees, constraint_model, all_shifts)
     add_employees_can_work_only_shifts_that_they_trained_for_constraint(shifts, employees, constraint_model, all_shifts)
 
     solver = cp_model.CpSolver()
-    solution_printer = VarArraySolutionPrinter(list(afternoon_shift_keys.values()))
-    solver.parameters.enumerate_all_solutions = True
-    status = solver.Solve(constraint_model, solution_printer)
-
-    # solver = cp_model.CpSolver()
-    # status = solver.Solve(constraint_model)
+    status = solver.Solve(constraint_model)
 
     if status == cp_model.OPTIMAL:
         for employee in employees:
@@ -45,7 +42,7 @@ def create_schedule(employees: list[Employee], shifts: list[Shift]) -> list[tupl
 
     if schedule:
         shift_index = 1
-        schedule.sort(key= lambda shift_employee_pair: shift_employee_pair[shift_index].start_time.date())
+        schedule.sort(key= lambda shift_employee_pair: shift_employee_pair[shift_index].start_time)
         return schedule
     else:
         raise Exception("Empty schedule. No Optimal solution found.")
