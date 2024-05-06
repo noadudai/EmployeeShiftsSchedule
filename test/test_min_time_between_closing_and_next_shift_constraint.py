49334e0642a67a6f3b3a840ada003a5a02328a6e
Noa Dudai <nooninana@gmail.com>
2024-05-06 17:12:15 +0300

    fixed names

--- test/test_min_time_between_closing_and_next_shift_constraint.py
+++ test/test_min_time_between_closing_and_next_shift_constraint.py
@@ -26,7 +26,7 @@ def test_no_optimal_solution_when_the_closing_shift_and_the_next_shift_are_too_c
 
     closing_shift = Shift(shift_id=uuid4(), shift_type=ShiftTypesEnum.CLOSING, start_time=start_closing_shift_time , end_time=end_closing_shift_time)
 
-    # the forbidden shift starts 1 hour before the minimum time between the shifts passed
+    # the forbidden shift starts 3 hour before the minimum time between the shifts passed
     start_forbidden_shift_time = closing_shift.end_time + (minimum_time_between_shifts - datetime.timedelta(hours=3))
     shift_too_close_to_closing_shift = Shift(shift_id=uuid4(), shift_type=ShiftTypesEnum.MORNING, start_time=start_forbidden_shift_time, end_time=start_forbidden_shift_time + datetime.timedelta(random.random()))
 
@@ -37,7 +37,6 @@ def test_no_optimal_solution_when_the_closing_shift_and_the_next_shift_are_too_c
     all_shifts = generate_shift_employee_combinations(employees, shifts, model)
 
     add_exactly_one_employee_per_shift_constraint(shifts, employees, model, all_shifts)
-    # add_at_most_one_shift_per_employee_in_the_same_day_constraint(shifts, employees, model, all_shifts)
     afternoon_start_time = datetime.time(12, 30)
     add_minimum_time_between_a_morning_shift_and_the_shift_before_constraint(shifts, employees, model, all_shifts, minimum_time_between_shifts, afternoon_start_time)
 
@@ -66,7 +65,6 @@ def test_every_employee_that_worked_closing_shift_does_not_work_the_next_shifts_
     all_shifts = generate_shift_employee_combinations(employees, shifts, model)
 
     add_exactly_one_employee_per_shift_constraint(shifts, employees, model, all_shifts)
-    add_at_most_one_shift_per_employee_in_the_same_day_constraint(shifts, employees, model, all_shifts)
     afternoon_start_time = datetime.time(12, 30)
     add_minimum_time_between_a_morning_shift_and_the_shift_before_constraint(shifts, employees, model, all_shifts, minimum_time_between_shifts, afternoon_start_time)
 
@@ -78,6 +76,7 @@ def test_every_employee_that_worked_closing_shift_does_not_work_the_next_shifts_
 def test_all_the_employees_who_worked_in_the_afternoon_are_not_working_in_the_morning_of_the_day_after():
     minimum_time_between_shifts = datetime.timedelta(hours=9)
     shift_duration = datetime.timedelta(minutes=random.random())
+
     # up to 29 because there are months that do not go up to 31 or even 30 days.
     morning_shift_start_time = datetime.datetime(2024, random.randint(1, 12), random.randint(1, 29), 7, 30)
     afternoon_shift_end_time = morning_shift_start_time - (minimum_time_between_shifts/2)
@@ -96,8 +95,9 @@ def test_all_the_employees_who_worked_in_the_afternoon_are_not_working_in_the_mo
     all_shifts = generate_shift_employee_combinations(employees, shifts, model)
 
     afternoon_employee_working_evening_key = ShiftCombinationsKey(afternoon_employee.employee_id, afternoon_shift.shift_id)
-    morning_employee_working_evening_key = ShiftCombinationsKey(morning_employee.employee_id, morning_shift.shift_id)
+    morning_employee_working_morning_key = ShiftCombinationsKey(morning_employee.employee_id, morning_shift.shift_id)
 
+    # manually assigning the afternoon employee to the evening shift
     model.Add(all_shifts[afternoon_employee_working_evening_key] == 1)
 
     add_exactly_one_employee_per_shift_constraint(shifts, employees, model, all_shifts)
@@ -109,4 +109,4 @@ def test_all_the_employees_who_worked_in_the_afternoon_are_not_working_in_the_mo
     status = solver.Solve(model)
     assert (status == cp_model.OPTIMAL)
 
-    assert solver.Value(all_shifts[morning_employee_working_evening_key]) == True
+    assert solver.Value(all_shifts[morning_employee_working_morning_key]) == True
