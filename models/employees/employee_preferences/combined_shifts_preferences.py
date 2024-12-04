0d4ed7a429fdc01081cfa375785987e72b9414b5
Noa Dudai <nooninana@gmail.com>
2024-12-04 23:06:52 +0200

    unnecessary

--- models/employees/employee_preferences/combined_shifts_preferences.py
+++ models/employees/employee_preferences/combined_shifts_preferences.py
@@ -2,14 +2,10 @@ from dataclasses import dataclass, field
 
 from models.employees.employee_preferences.combine_preference import CombinePreferences
 from models.employees.employee_preferences.shifts_preferences import ShiftsPreferences
-from models.shifts.shift import Shift
 
 
 @dataclass
-class CombinedShiftsPreferences(ShiftsPreferences):
+class CombinedShiftsPreferences:
     shifts_cannot_work: ShiftsPreferences = field(default_factory=CombinePreferences)
     shifts_prefer_not_to_work: ShiftsPreferences = field(default_factory=CombinePreferences)
     shifts_wants_to_work: ShiftsPreferences = field(default_factory=CombinePreferences)
-
-    def get_shifts_preferences(self, shifts: list[Shift]) -> list[Shift]:
-        pass
