7f1714f09d5b3da246f9454515e261f4b0d5d114
Noa Dudai <nooninana@gmail.com>
2025-01-19 15:21:03 +0200

    att

--- src/models/employees/employee_preferences/shifts_preference_by_id.py
+++ src/models/employees/employee_preferences/shifts_preference_by_id.py
@@ -10,7 +10,7 @@ from src.models.solution.pydantic_config import ConfigPydanticDataclass
 
 @pydantic.dataclasses.dataclass(config=ConfigPydanticDataclass)
 class ShiftIdPreference(ShiftsPreference):
-    shifts_pref_by_id: list[uuid] = field(default_factory=list)
+    shifts_pref_by_id: list[str] = field(default_factory=list)
 
     def get_shifts_preference(self, shifts: list[Shift]) -> list[Shift]:
         shifts = [shift for shift in shifts if shift.shift_id in self.shifts_pref_by_id]
