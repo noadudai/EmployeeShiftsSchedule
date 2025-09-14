8fe63a50f42925e0b7a840c817da79ad15c22b6e
Noa Dudai <nooninana@gmail.com>
2025-09-14 15:14:07 +0300

    receive the employees, shifts and number of schedules

---
+++ src/models/request_models/schedule_creation_data.py
@@ -0,0 +1,12 @@
+import pydantic
+
+from src.models.employees.employee import Employee
+from src.models.shifts.shift import Shift
+from src.models.solution.pydantic_config import ConfigPydanticDataclass
+
+
+@pydantic.dataclasses.dataclass(config=ConfigPydanticDataclass)
+class ScheduleCreationData:
+    employees: list[Employee]
+    shifts: list[Shift]
+    number_of_schedules: int = 5
