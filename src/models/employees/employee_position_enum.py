f3397f1b8a472312e10fde45f2a5bdad77e5a3e5
Noa Dudai <nooninana@gmail.com>
2025-10-08 15:21:38 +0300

    maybe

--- src/models/employees/employee_position_enum.py
+++ src/models/employees/employee_position_enum.py
@@ -1,11 +1,11 @@
 from enum import Enum
 
 EmployeePositionValue = {
-    "PART_TIMER": 1,
-    "FULL_TIMER": 3
+    "PartTimer": 1,
+    "FullTimer": 3
 }
 
 
 class EmployeePositionEnum(Enum):
-    PART_TIMER = "PART_TIMER"
-    FULL_TIMER = "FULL_TIMER"
+    PART_TIMER = "PartTimer"
+    FULL_TIMER = "FullTimer"
